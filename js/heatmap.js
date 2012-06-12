100cf616321369eed710db9f04ddf67f31116f6a
Petrea Corneliu Stefan <stefan.petrea@gmail.com>
2012-06-12 20:22:21 +0300

    added some more code

--- js/heatmap.js
+++ js/heatmap.js
@@ -5,15 +5,29 @@ require(["jquery"], function($) {
     $(function() {
 
       function HeatMap() {
-        this.url = "localhost/cgi-bin/
+        var that = this;
+        this.backendUrl = "http://localhost/rac-heatmap/test.pl";
         this.tellServer = function(x,y) {
-
+          var clickData = {
+            url: window.location.toString(),
+            x  : x,
+            y  : y,
+          };
+          $.ajax({
+            type    : 'POST',
+            url     : that.backendUrl,
+            data    : clickData,
+            success : function(e) {
+              console.log(e);
+            },
+          });
         };
 
         this.clickCallback = function(ev) {
             var x = ev.pageX;
             var y = ev.pageY;
             console.log(x+" "+y);
+            that.tellServer(x,y);
         };
 
 
--- test.pl
+++ test.pl
@@ -1,3 +1,23 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use JSON;
+use Data::Dumper;
+use Storable;
+use DateTime;
+use DBI;
+my $cgi = CGI->new;
 print "Content-type: text/html\n\n";
-print "<H1>Hello World</H1>\n";
+my $postdata = $cgi->param("POSTDATA");
+warn "<pre>$postdata</pre>";
+
+warn Dumper($cgi->{param});
+my $x   = $cgi->param("x"   );
+my $y   = $cgi->param("y"   );
+my $url = $cgi->param("url" );
+
+
+
+
+
